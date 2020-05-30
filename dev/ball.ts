a0dce7d427c7118200acf151eecce8838c26bc63
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-05-30 14:14:09 +0200

    Add value to x & y + add speed variable.

--- dev/ball.ts
+++ dev/ball.ts
@@ -1,8 +1,11 @@
 class Ball {
     
     private div : HTMLElement
-    private x : number
-    private y : number
+    private x : number = 0
+    private y : number = 0
+    private xSpeed : number = 0
+    private ySpeed : number = 0
+
     
     constructor() {
         this.div = document.createElement("ball")
@@ -12,9 +15,17 @@ class Ball {
 
         this.x = Math.random() * window.innerWidth
         this.y = Math.random() * window.innerHeight
+
+        this.xSpeed = 2
+        this.ySpeed = -3
+        
     }
     
     public update() : void {
+
+        this.x += this.xSpeed
+        this.y += this.ySpeed
+
         this.div.style.transform = `translate(${this.x}px, ${this.y}px)`
     }
 }
