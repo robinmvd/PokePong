13b2cf5373ec45c537316b563e283974b90e11c9
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-05-30 15:28:18 +0200

    Add point to score.

--- dev/game.ts
+++ dev/game.ts
@@ -2,6 +2,7 @@ class Game {
     
     private balls : Ball[] = []
     private paddle : Paddle
+    private score : number = 0
     
     constructor() {
 
@@ -21,6 +22,7 @@ class Game {
             if (this.checkCollision(ball.getRectangle(), this.paddle.getRectangle())) {
                 console.log("BOTSING MET PADDLE")
                 ball.bounceX()
+                this.addPoint()
             }
         }
 
@@ -47,6 +49,13 @@ class Game {
             b.left <= a.right &&
             a.top <= b.bottom &&
             b.top <= a.bottom)
+     }
+
+     private addPoint() {
+         let score = document.getElementsByTagName("score")[0]
+
+         this.score++
+            score.innerHTML = "Score: "+this.score
      }
 } 
 
